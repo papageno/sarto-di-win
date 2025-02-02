13a3ed4baaab7e5cb303e9564e25bdc2bae7aa02
Papageno <14852702+papageno@users.noreply.github.com>
2025-02-03 05:37:27 +0900

    Update cue of sbin/setupcomplete.ps1

--- sbin/setupcomplete.ps1
+++ sbin/setupcomplete.ps1
@@ -5,15 +5,9 @@ Set-StrictMode -Off
 
 & "$PSScriptRoot\.\driver.ps1" -Driver "$PSScriptRoot\..\etc\driver.driver"
 & "$PSScriptRoot\..\bin\wifi.ps1" -Config "$PSScriptRoot\..\etc\wifi.config"
-& "$PSScriptRoot\.\printer.ps1" -Config "$PSScriptRoot\..\etc\printer.config"
 & "$PSScriptRoot\..\bin\connectivity.ps1"
-& "$PSScriptRoot\.\tattoo.ps1" -Config "$PSScriptRoot\..\etc\tattoo.config"
-& "$PSScriptRoot\..\bin\connectivity.ps1"
-& "$PSScriptRoot\.\time.ps1"
 & "$PSScriptRoot\.\user.ps1" -Config "$PSScriptRoot\..\etc\user.config"
+& "$PSScriptRoot\.\tattoo.ps1" -Config "$PSScriptRoot\..\etc\tattoo.config"
+& "$PSScriptRoot\.\printer.ps1" -Config "$PSScriptRoot\..\etc\printer.config"
+& "$PSScriptRoot\.\time.ps1"
 & "$PSScriptRoot\.\skipoobe.ps1"
-
-# If script is not called from another script
-if ($null -eq $MyInvocation.PSCommandPath) {
-    Restart-Computer -Confirm
-}
